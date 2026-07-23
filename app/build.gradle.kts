440b53dfbe6d4dc05b6424b296d7d1e90d906153
Jurian Onderwater <jurian.onderwater247@gmail.com>
2026-07-23 17:28:00 +0200

    Bump version to 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.example.flightstats"
         minSdk = 31
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
